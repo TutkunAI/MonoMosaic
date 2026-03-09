cd170fcc3609aec1b77621ad110235720f115dc8
Nemege16 <yanbasmayana751@gmail.com>
2026-03-09 15:35:37 +0300

    Add spinning cube demo and modify renderTask function

---
+++ MonoMosaicV1.0/MonoMosaicV1.0.ino
@@ -0,0 +1,12 @@
+// Code for spinning cube demo
+void drawSpinningCube() {
+    // 3D rotation functions and cube drawing logic
+    // Implement 3D rotation transformation here
+}
+
+// Invoke drawing function in the render task
+void renderTask() {
+    // existing render logic...
+    drawSpinningCube();
+    // existing render logic...
+}
